0a8d19124599a52b0f5a21b27c62493332160eb7
MSlezak2 <37062938+MSlezak2@users.noreply.github.com>
2020-07-26 16:15:59 +0200

    Update GUI class
    The frame has been divided into two panels. One for details, the other for universe displaying.

--- src/life/view/GameOfLife.java
+++ src/life/view/GameOfLife.java
@@ -1,24 +1,60 @@
 package life.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameOfLife extends JFrame {
 
-    JPanel detailPanel;
-    JPanel universePanel;
-    int frameWidth = 600;
-    int framwHeight = 700;
+    private JPanel detailsPanel;
+    private JPanel universePanel;
+    private JLabel currentGenerationLabel;
+    private JLabel aliveNumberLabel;
+    private int frameWidth = 600;
+    private int frameHeight = 700;
 
     public GameOfLife() {
 
-        setupFrame(frameWidth,framwHeight);
+        setupFrame(frameWidth, frameHeight);
+        setupDetailsPanel();
+        setupUniversePanel();
 
     }
 
     private void setupFrame(int width, int height) {
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(width,height);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        setSize(width, height);
         setVisible(true);
+        repaint();      // without those lines
+        revalidate();   // gui is displayed unpredictably
+
+    }
+
+    private void setupDetailsPanel() {
+
+        detailsPanel = new JPanel(new GridLayout(2, 1));
+        detailsPanel.setBackground(new Color(150, 200, 200));
+        detailsPanel.setPreferredSize(new Dimension(this.getWidth(), (int) (this.getHeight() * 0.1)));
+
+        currentGenerationLabel = new JLabel("Generation #");
+        detailsPanel.add(currentGenerationLabel);
+
+        aliveNumberLabel = new JLabel("Alive: ");
+        detailsPanel.add(aliveNumberLabel);
+
+        add(detailsPanel);
+
+    }
+
+    private void setupUniversePanel() {
+
+        universePanel = new JPanel();
+        universePanel.setBackground(new Color(150, 175, 150));
+        universePanel.setPreferredSize(new Dimension(this.getWidth(), (int) (this.getHeight() * 0.9)));
+
+        add(universePanel);
+
     }
 
 }
